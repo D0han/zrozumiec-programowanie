bb467bf9aeb59ae29755d97ac3c5293f33cbfe2e
D0han <piotr.szczepaniak@nokia.com>
2016-07-20 18:18:20 +0200

    code is now a dict!

--- 007-Czesc_II-Rozdzial_3-Podstawy_architektury_komputerowe/disasm.py
+++ 007-Czesc_II-Rozdzial_3-Podstawy_architektury_komputerowe/disasm.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 import sys
+from collections import defaultdict
 import vm_instr
 
 
 def disasm(binary):
+    code = defaultdict(lambda: [])
     labels = {}
     pc = 0
 #    print vm_instr.VM_OPCODES
@@ -12,7 +14,7 @@ def disasm(binary):
     while pc < len(binary):
         byte = binary[pc]
         if byte not in vm_instr.VM_OPCODES:
-            print "%.4x:      db   \t0x%.2x\t; %s" % (pc, byte, chr(byte))
+            code[pc].append("db   \t0x%.2x\t; %s" % (byte, chr(byte)))
             pc += 1
             continue
         instr = vm_instr.VM_OPCODES[byte]
@@ -27,33 +29,38 @@ def disasm(binary):
         # reverse arguments order for some instructions
         if instr_name in ['VCRL', 'VCRS', 'VOUTB', 'VINB']:
             params = params[::-1]
+        # skaczemy?
         if instr_name.startswith('VJ'):
-            tmp = int(''.join('%.2x' % x for x in params[0]), 16)
+            code[pc].append(str(params))
+            jump_to = int(''.join(['%.2x' % x for y in params for x in y]), 16) + pc
+            if jump_to not in labels:
+                labels[jump_to] = 'label%i' % len(labels)
             if instr_name != 'VJMPR':
                 #tmp += pc
                 pass
-            print tmp
-            params[0][1] = tmp
+            code[pc].append(str(jump_to))
+            code[jump_to].insert(0, '%s:' % labels[jump_to])
 
-        print "%.4x: 0x%.2x %s  " % (pc, byte, instr_name.lower()),
+        code[pc].append("%s  " % instr_name.lower())
+        instr_correct = True
         if len(params):
             params_s = []
             for param in params:
-                if not param:
-                    continue
+                if not param: # failed to read all params
+                    instr_correct = False
+                    code[pc][-1] = "db   \t0x%.2x\t; %s" % (byte, chr(byte))
+                    pc += 1
+                    break
                 params_s.append('0x%x' % int(''.join(['%.2x' % x for x in param]), 16))
-            print "\t%s" % ', '.join(params_s)
-        else:
-            print ""
+            else:
+                code[pc][-1] += "\t%s" % ', '.join(params_s)
 
-        # skaczemy?
-        if instr_name.startswith('VJ'):
-            print params
-            jump_to = int(''.join(['%.2x' % x for y in params for x in y]), 16)
-            if jump_to not in labels:
-                labels[jump_to] = 'label%i' % len(labels)
-
-        pc += instr[1]+1
+        if instr_correct:
+            pc += instr[1]+1
+    print '%include "vm.inc"\n'
+    for addr in sorted(code.keys()):
+        for line in code[addr]:
+            print "%.4x: %s" % (addr, line)
     print '>>> END'
     print labels
 
