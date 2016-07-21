04b80d17ee55c4866e6f58e3a5199a44065aa9b8
D0han <d0hanzibi@gmail.com>
2016-07-21 23:46:11 +0200

    refactor

--- 007-Czesc_II-Rozdzial_3-Podstawy_architektury_komputerowe/disasm.py
+++ 007-Czesc_II-Rozdzial_3-Podstawy_architektury_komputerowe/disasm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import string
 from collections import defaultdict
 
 
@@ -51,63 +52,84 @@ VM_OPCODES = {
 }
 
 
-def disasm(binary):
-    code = defaultdict(lambda: [])
-    labels = {}
-    pc = 0
-#    print binary
-    while pc < len(binary):
-        byte = binary[pc]
-        if byte not in VM_OPCODES:
-            code[pc].append("db   \t0x%.2x\t; %s" % (byte, chr(byte)))
-            pc += 1
-            continue
-        instr = VM_OPCODES[byte]
+class disasm(object):
+    def __init__(self, binary, vm_opcodes):
+        self.binary = binary
+        self.vm_opcodes = vm_opcodes
+        self.code = defaultdict(lambda: [])
+        self.pc = 0
+        self.labels = {}
+        self.instr = None
+
+    def _handle_unknown_opcode(self):
+        byte = self.binary[self.pc]
+        if chr(byte) in set(string.printable) - set(string.whitespace):
+            readable = chr(byte)
+        else:
+            readable = ''
+        self.code[self.pc].append("db   \t0x%.2x\t; %s" % (byte, readable))
+        self.pc += 1
+
+    def _read_params(self):
         params = []
         params_bytes_readed = 0
-        for arg_len in instr['params']:
+        for arg_len in self.instr['params']:
             if not arg_len:
                 continue
-            params.append(binary[pc+arg_len+params_bytes_readed:pc+params_bytes_readed:-1])
+            params.append(self.binary[self.pc+arg_len+params_bytes_readed:
+                                      self.pc+params_bytes_readed:-1])
             params_bytes_readed += arg_len
         # reverse arguments order for some instructions
-        if instr['reverse']:
+        if self.instr['reverse']:
             params = params[::-1]
-        # skaczemy?
-        if instr['name'].startswith('VJ'):
-            code[pc].append(str(params))
-            jump_to = int(''.join(['%.2x' % x for y in params for x in y]), 16) + \
-                      pc + 1 + sum(instr['params'])
-            if jump_to not in labels:
-                labels[jump_to] = 'label%i' % len(labels)
-            if instr['name'] != 'VJMPR':
-                #tmp += pc
-                pass
-            code[pc].append(str(jump_to))
-            code[jump_to].insert(0, '%s:' % labels[jump_to])
+        return params
 
-        code[pc].append("%s  " % instr['name'].lower())
+    def _write_mnemonic(self, params):
         instr_correct = True
-        if len(params):
-            params_s = []
-            for param in params:
-                if not param: # failed to read all params
-                    instr_correct = False
-                    code[pc][-1] = "db   \t0x%.2x\t; %s" % (byte, chr(byte))
-                    pc += 1
-                    break
-                params_s.append('0x%x' % int(''.join(['%.2x' % x for x in param]), 16))
-            else:
-                code[pc][-1] += "\t%s" % ', '.join(params_s)
+        params_s = []
+        for param in params:
+            if not param:  # failed to read all params
+                instr_correct = False
+                self._handle_unknown_opcode()
+                break
+            params_s.append('0x%x' % int(''.join(['%.2x' % x for x in param]), 16))
 
         if instr_correct:
-            pc += sum(instr['params'])+1
-    print '%include "vm.inc"\n'
-    for addr in sorted(code.keys()):
-        for line in code[addr]:
-            print "%.4x: %s" % (addr, line)
-    print '>>> END'
-    print labels
+            self.code[self.pc].append("%s  \t%s" % (self.instr['name'].lower(), ', '.join(params_s)))
+            self.pc += sum(self.instr['params'])+1
+
+    def _handle_jump(self, params):
+        self.code[self.pc].append(str(params))
+        jump_to = int(''.join(['%.2x' % x for y in params for x in y]), 16) + \
+                  1 + sum(self.instr['params'])
+        if self.instr['name'] != 'VJMPR':  # this jump is not relative
+            jump_to += self.pc
+        if jump_to not in self.labels:
+            self.labels[jump_to] = 'label%i' % len(self.labels)
+        self.code[self.pc].append(str(jump_to))
+        self.code[jump_to].insert(0, '%s:' % self.labels[jump_to])
+
+    def analyze(self):
+        while self.pc < len(self.binary):
+            byte = self.binary[self.pc]
+            if byte not in self.vm_opcodes:
+                self._handle_unknown_opcode()
+                continue
+            self.instr = self.vm_opcodes[byte]
+            params = self._read_params()
+            # jumping?
+            if self.instr['name'].startswith('VJ'):
+                self._handle_jump(params)
+            self._write_mnemonic(params)
+
+    def print_out(self):
+        print '%include "vm.inc"\n'
+        for addr in sorted(self.code.keys()):
+            for line in self.code[addr]:
+                print "%.4x: %s" % (addr, line)
+        print '>>> END'
+        print self.labels
+
 
 def main():
     if len(sys.argv) != 2:
@@ -116,7 +138,9 @@ def main():
     with open(sys.argv[1], 'rb') as binary:
         data = [byte.encode('hex') for byte in binary.read()]
         data = [int(byte, 16) for byte in data]
-        disasm(data)
+        dis = disasm(data, VM_OPCODES)
+        dis.analyze()
+        dis.print_out()
 
 if __name__ == '__main__':
     main()
